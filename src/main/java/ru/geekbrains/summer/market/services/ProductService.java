2f0199689fbe2ead96b1d2fa1f83f123eae5cf46
MigSer13 <avto470@mail.ru>
2021-09-07 22:34:45 +0300

    market

---
+++ src/main/java/ru/geekbrains/summer/market/services/ProductService.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.summer.market.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.summer.market.model.Product;
+import ru.geekbrains.summer.market.repositories.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Page<Product> findPage(int pageIndex, int pageSize, Specification<Product> spec) {
+        return productRepository.findAll(spec, PageRequest.of(pageIndex, pageSize));
+    }
+
+    public Product save(Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
